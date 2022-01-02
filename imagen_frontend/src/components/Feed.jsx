3d273b7e62ea984b59bd5ca1fab2f78d0c827556
Arnav Sharma <arnavsharma2711@gmail.com>
2022-01-02 23:39:21 +0530

    Deployment

---
+++ imagen_frontend/src/components/Feed.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+const Feed = () => {
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+  const ideaName = categoryId || 'new';
+  let title = categoryId ? `${categoryId} | ` : '';
+  if (categoryId) title = title.charAt(0).toUpperCase() + title.slice(1);
+  if (loading) {
+    return (
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+  }
+  return (
+    <div>
+      <Helmet>
+        <title>{`${title}Imágen | A Photo Sharing App`}</title>
+      </Helmet>
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
+    </div>
+  );
+};
+
+export default Feed;
